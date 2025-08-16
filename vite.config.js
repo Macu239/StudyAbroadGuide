af5fe971d0239653d93674e4f400ea2ac2c7bfe0
Jeremy Shih <Jeremy02012005@gmail.com>
2025-08-16 12:39:55 +0800

    set up dist

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, '../server/dist'),
+    outDir: 'dist', // now build stays inside FrontEnd
     emptyOutDir: true
   }
 });
